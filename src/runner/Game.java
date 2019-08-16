ff51d061edcc8693452495045959505830c060e9
Shotim <39776975+Shotim@users.noreply.github.com>
2019-08-16 22:47:32 +0300

    added singleplayer

--- src/runner/Game.java
+++ src/runner/Game.java
@@ -4,7 +4,15 @@ import scanner.EnterFromConsole;
 
 public class Game {
 
-    boolean isRunning = true;
+    private static final int BOT = 1;
+    private static final int HUMAN = -1;
+    private static final boolean COMPLETE = true;
+    private static final boolean INCOMPLETE = false;
+    private static final int PLAYER_VS_PLAYER_MODE = 1;
+    private static final int PLAYER_VS_COMP_MODE = 2;
+    private static final int EXIT = 3;
+
+    private boolean isRunning = true;
 
     public void menu() {
 
@@ -14,14 +22,13 @@ public class Game {
             System.out.println("2 - SinglePlayer");
             System.out.println("3 - Exit\n");
             System.out.print("Your choice: ");
-            switch (EnterFromConsole.enterInt()) {
-                case 1:
-                    gameModePlayerVSPlayer();
+            int choice = EnterFromConsole.enterInt();
+            switch (choice) {
+                case PLAYER_VS_PLAYER_MODE:
+                case PLAYER_VS_COMP_MODE:
+                    gameMode(choice);
                     break;
-                case 2:
-                    gameModePlayerVSComp();
-                    break;
-                case 3:
+                case EXIT:
                     isRunning = false;
                     break;
                 default:
@@ -30,39 +37,54 @@ public class Game {
         }
     }
 
-    void gameModePlayerVSPlayer() {
+    private void gameMode(int gameMode) {
 
         Cell[][] field = PlayingField.create();
         Player player = new Player();
 
         boolean exit = false;
+        if (gameMode == PLAYER_VS_COMP_MODE) {
+            System.out.println("Do you want to go first?\n1 - Yes, I want to be 'X'\n2 - No, I want to be 'O'");
+            int choice = EnterFromConsole.enterInt();
+            if (choice == 2) {
+                player.markTheCell(field[field.length / 2][field.length / 2]);
+                player.turnChange();
+            }
+        }
         while (!exit) {
             PlayingField.illustrate(field);
             player.makeAMove(field);
-            if (winCheck(field)) {
-                exit = true;
-                PlayingField.illustrate(field);
-                System.out.println("The winner is the " + player.playerTurn + "-th player!!!\n\n\n");
-            } else if (PlayingField.isFilled(field)) {
-                exit = true;
-                PlayingField.illustrate(field);
-                System.out.println("DRAW!!!\n\n\n");
-            } else player.turnChange();
+            exit = exit(field, player);
+            if (gameMode == PLAYER_VS_COMP_MODE) {
+                gameAlgorithm(field, player);
+                exit = exit(field, player);
+            }
         }
     }
 
-    void gameModePlayerVSComp() {
-        Cell[][] field = PlayingField.create();
+    private boolean exit(Cell[][] field, Player player) {
+        if (winCheck(field)) {
+            PlayingField.illustrate(field);
+            System.out.println("The winner is the " + player.playerTurn + "-th player!!!\n\n\n");
+            return true;
+        } else if (PlayingField.isFilled(field)) {
+            PlayingField.illustrate(field);
+            System.out.println("DRAW!!!\n\n\n");
+            return true;
+        } else {
+            player.turnChange();
+            return false;
+        }
     }
 
-    boolean lineIsEquallyFilled(Cell[] line) {
+    private boolean lineIsEquallyFilled(Cell[] line) {
         for (int i = 1; i < line.length; i++) {
             if (line[i].status != line[i - 1].status || line[i].isEmpty()) return false;
         }
         return true;
     }
 
-    boolean winCheck(Cell[][] field) {
+    private boolean winCheck(Cell[][] field) {
         if (lineIsEquallyFilled(PlayingField.diagonal(field, PlayingField.LEFT))) return true;
         else if (lineIsEquallyFilled(PlayingField.diagonal(field, PlayingField.RIGHT))) return true;
         else {
@@ -73,5 +95,61 @@ public class Game {
         }
         return false;
     }
+
+    private boolean gameAlgorithm(Cell[][] field, Player player) {
+        if (findingWinSituation(field, player, BOT)) return COMPLETE;
+        else if (findingWinSituation(field, player, HUMAN)) return COMPLETE;
+        else if (field[field.length / 2][field.length / 2].isEmpty())
+            player.markTheCell(field[field.length / 2][field.length / 2]);
+        else if (findingAngleToMark(field, player)) return COMPLETE;
+        else findingAnyCellToMark(field, player);
+        return COMPLETE;
+    }
+
+    private boolean findingWinSituation(Cell[][] field, Player player, int side) {
+        if (side == HUMAN) player.turnChange();
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j].isEmpty()) {
+                    player.markTheCell(field[i][j]);
+                    if (winCheck(field)) {
+                        if (side == HUMAN) {
+                            player.turnChange();
+                            player.markTheCell(field[i][j]);
+                        }
+                        return COMPLETE;
+                    }
+                    player.unmarkTheCell(field[i][j]);
+                }
+            }
+        }
+        if (side == HUMAN) player.turnChange();
+        return INCOMPLETE;
+    }
+
+    private boolean findingAngleToMark(Cell[][] field, Player player) {
+        for (int i = 0; i < field.length; i += field.length - 1) {
+            for (int j = 0; j < field.length; j += field.length - 1) {
+                if (field[i][j].isEmpty()) {
+                    player.markTheCell(field[i][j]);
+                    return COMPLETE;
+                }
+            }
+        }
+        return INCOMPLETE;
+    }
+
+    private boolean findingAnyCellToMark(Cell[][] field, Player player) {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field.length; j++) {
+                if (field[i][j].isEmpty()) {
+                    player.markTheCell(field[i][j]);
+                    return COMPLETE;
+                }
+            }
+        }
+        return INCOMPLETE;
+    }
+
 
 }
